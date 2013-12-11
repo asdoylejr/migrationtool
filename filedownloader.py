35ba384db48eb0ca8fb3166a8de18c9983253396
asdoylejr <asdoylejr@gmail.com>
2013-12-11 09:08:17 -0500

    removed links added base url

--- filedownloader.py
+++ filedownloader.py
@@ -5,51 +5,16 @@ from re import compile
 import os
 import sys
 
-URLs = ['http://www1.eere.energy.gov/water/',
-'http://www1.eere.energy.gov/water/about.html',
-'http://www1.eere.energy.gov/water/activities.html',
-'http://www1.eere.energy.gov/library/pir_publicationsnew.aspx/page/14',
-'http://www1.eere.energy.gov/water/budget.html',
-'http://www1.eere.energy.gov/water/program_peer_reviews.html',
-'http://www1.eere.energy.gov/water/2011_peer_review.html',
-'http://www1.eere.energy.gov/water/contacts.html',
-'http://www1.eere.energy.gov/water/research.html',
-'http://www1.eere.energy.gov/water/hydropower.html',
-'http://www1.eere.energy.gov/water/hydro_technology_development.html',
-'http://www1.eere.energy.gov/water/hydro_market_acceleration.html',
-'http://www1.eere.energy.gov/water/hydro_assessment_characterization.html',
-'http://www1.eere.energy.gov/water/marine_hydrokinetic.html',
-'http://www1.eere.energy.gov/water/marine_technology_development.html',
-'http://www1.eere.energy.gov/water/marine_market_acceleration.html',
-'http://www1.eere.energy.gov/water/marine_assessment_characterization.html',
-'http://www1.eere.energy.gov/water/financial_opportunities.html',
-'http://www1.eere.energy.gov/water/past_opportunities.html',
-'http://www1.eere.energy.gov/water/related_opportunities.html',
-'http://www1.eere.energy.gov/water/resources.html',
-'http://www1.eere.energy.gov/water/hydropower_resources.html',
-'http://www1.eere.energy.gov/water/how_hydropower_works.html',
-'http://www1.eere.energy.gov/water/hydro_plant_types.html',
-'http://www1.eere.energy.gov/water/hydro_turbine_types.html',
-'http://www1.eere.energy.gov/water/hydro_benefits.html',
-'http://www1.eere.energy.gov/water/hydro_history.html',
-'http://www1.eere.energy.gov/water/hydro_glossary.html',
-'http://www1.eere.energy.gov/water/marine_hydro_resources.html',
-'http://www1.eere.energy.gov/water/marine_hydro_glossary.html',
-'http://www1.eere.energy.gov/water/related_links.html',
-'http://www1.eere.energy.gov/water/news.html',
-'http://www1.eere.energy.gov/water/subscribe.html',
-'http://www1.eere.energy.gov/water/events.html',
-'http://www1.eere.energy.gov/water/contacts_landing.html',
-'http://www1.eere.energy.gov/water/webmaster.html',
-]
+URLs = []
+BASEURL = ''
 
 def pdfs():
 	for link in soup.findAll(href = compile('\.pdf$')):
 		file = link.get('href')
 		filename = file.split('/')[-1]
 		if file[0] == "/":
-			urlretrieve('http://www1.eere.energy.gov' + file, filename)
-			print 'http://www1.eere.energy.gov' + file
+			urlretrieve(BASEURL + file, filename)
+			print BASEURL + file
 		else:
 			urlretrieve(file, filename)
 			print file
@@ -59,8 +24,8 @@ def docs():
 		file = link.get('href')
 		filename = file.split('/')[-1]
 		if file[0] == "/":
-			urlretrieve('http://www1.eere.energy.gov' + file, filename)
-			print 'http://www1.eere.energy.gov' + file
+			urlretrieve(BASEURL + file, filename)
+			print BASEURL + file
 		else:
 			urlretrieve(file, filename)
 			print file
@@ -70,8 +35,8 @@ def excel():
 		file = link.get('href')
 		filename = file.split('/')[-1]
 		if file[0] == "/":
-			urlretrieve('http://www1.eere.energy.gov' + file, filename)
-			print 'http://www1.eere.energy.gov' + file
+			urlretrieve(BASEURL + file, filename)
+			print BASEURL + file
 		else:
 			urlretrieve(file, filename)
 			print file
@@ -81,8 +46,8 @@ def jpg():
 		file = link.get('src')
 		filename = file.split('/')[-1]
 		if file[0] == "/":
-			urlretrieve('http://www1.eere.energy.gov' + file, filename)
-			print 'http://www1.eere.energy.gov' + file
+			urlretrieve(BASEURL + file, filename)
+			print BASEURL + file
 		else:
 			urlretrieve(file, filename)
 			print file
@@ -92,8 +57,8 @@ def png():
 		file = link.get('src')
 		filename = file.split('/')[-1]
 		if file[0] == "/":
-			urlretrieve('http://www1.eere.energy.gov' + file, filename)
-			print 'http://www1.eere.energy.gov' + file
+			urlretrieve(BASEURL + file, filename)
+			print BASEURL + file
 		else:
 			urlretrieve(file, filename)
 			print file
