63878a42dd7a5e9d9c882f24b52deed8acc7bd12
asdoylejr <asdoylejr@gmail.com>
2013-12-11 21:57:45 -0500

    converted to function

--- pdfdataextractor.py
+++ pdfdataextractor.py
@@ -6,17 +6,21 @@ from pyPdf import PdfFileReader
 BASEDIR = 'Water_Files/'
 PDFFiles = ['11_wwf_principle_power_weinstein.pdf']
 
-for file in PDFFiles:
-	"""
-	Processes the files in the PDFFiles list.  If the document 
-	is missing data, it will pass and continue so you can 
-	inspect the exceptions by hand.
-	"""
-	try:
-		pdf_toread = PdfFileReader(open(BASEDIR + file, 'rb'))
-		pdf_info = pdf_toread.getDocumentInfo()
-		#print str(pdf_info)   #print full metadata if you want
-		print file + "--" + pdf_info['/Title'] + " - " + pdf_info['/Subject']
-	except:
-		print 'null'
-		pass
+def extractor():
+	for file in PDFFiles:
+		"""
+		Processes the files in the PDFFiles list.  If the document 
+		is missing data, it will pass and continue so you can 
+		inspect the exceptions by hand.
+		"""
+		try:
+			pdf_toread = PdfFileReader(open(BASEDIR + file, 'rb'))
+			pdf_info = pdf_toread.getDocumentInfo()
+			#print str(pdf_info)   #print full metadata if you want
+			print file + "--" + pdf_info['/Title'] + " - " + pdf_info['/Subject']
+		except:
+			print file + ' ' + ' ERROR: Data missing or corrupt'
+			pass
+
+if __name__ == "__main__":
+	extractor()
