a6ba8671ac1de6423bef9afbf8f11fd0cb5b49c3
asdoylejr <asdoylejr@gmail.com>
2013-12-16 17:37:23 -0500

    trimmed the fat

--- filedownloader.py
+++ filedownloader.py
@@ -1,69 +1,181 @@
 from BeautifulSoup import BeautifulSoup
-from urllib import urlretrieve
-from urllib2 import urlopen
+from urllib import urlretrieve, urlopen
+#from urllib2 import urlopen
 from re import compile
 import os
 import sys
 
-URLs = ['http://www1.eere.energy.gov/water/']
-BASEURL = ['http://www1.eere.energy.gov', 
-'http://apps1.eere.energy.gov']
-#FILETYPE = ['.pdf', '.jpg', '.png']
-#TAG = ['href', 'src']
+URLs = ['http://www1.eere.energy.gov/water/2011_peer_review.html']
+BASEURL = ['http://www1.eere.energy.gov', 'http://www1.eere.energy.gov/water/']
+FILETYPE = ['\.pdf$','\.ppt$', '\.pptx$']
 
-def download(arg1, arg2):
-	if arg1[0] == "/":
-		try:
-			urlretrieve(BASEURL[0] + arg1, arg2)
-			print BASEURL[0] + arg1
-		except:
-			urlretrieve(BASEURL[1] + arg1, arg2)
-			print BASEURL[1] + arg1
-	else:
-		urlretrieve(arg1, arg2)
-		print arg1
-
-def pdfs():
-	for link in soup.findAll(href = compile('\.pdf$')):
-		file = link.get('href')
-		filename = file.split('/')[-1]
-			
-		download(file, filename)
-	
-def docs():
-	for link in soup.findAll(href = compile('\.doc$')):
-		file = link.get('href')
-		filename = file.split('/')[-1]
-	
-	download(file, filename)
-
-def excel():
-	for link in soup.findAll(href = compile('\.xls$')):
-		file = link.get('href')
-		filename = file.split('/')[-1]
-	
-	download(file, filename)
-
-def jpg():
-	for link in soup.findAll(src = compile('\.jpg$')):
-		file = link.get('src')
-		filename = file.split('/')[-1]
-	
-	download(file, filename)
-
-def png():
-	for link in soup.findAll(src = compile('\.png$')):
-		file = link.get('src')
-		filename = file.split('/')[-1]
-	
-	download(file, filename)
-
+def pdf():
+        for link in soup.findAll(href = compile(types)):
+                file = link.get('href')
+                filename = file.split('/')[-1]
+                if file[0] == "/":
+                        #urlretrieve(BASEURL[0] + file, filename)
+                        print BASEURL[0] + file
+                elif file [0:2] == "pdf":
+                       # urlretrieve(BASEURL[1] + file, filename)
+                        print BASEURL[1] + file
+                else:
+                       # urlretrieve(file, filename)
+                        print file
 
 if __name__ == "__main__":
-	for url in URLs:
-		soup = BeautifulSoup(urlopen(url))
-		pdfs()
-		jpg()
-		png()
-		excel()
-		docs()
+        for url in URLs:
+                html_data = urlopen(url)
+                soup = BeautifulSoup(html_data)
+                
+                for types in FILETYPE:
+                        pdf()
+"""                       
+def ppt():
+        for link in soup.findAll(href = compile(FILETYPE[0])):
+                file = link.get('href')
+                filename = file.split('/')[-1]
+                if file[0] == "/":
+                        urlretrieve(BASEURL[0] + file, filename)
+                        print BASEURL[0] + file
+                elif file [0:2] == "pdf":
+                        urlretrieve(BASEURL[1] + file, filename)
+                        print BASEURL[1] + file
+                else:
+                        urlretrieve(file, filename)
+                        print file
+
+def pptx():
+        for link in soup.findAll(href = compile('\.pptx$')):
+                file = link.get('href')
+                filename = file.split('/')[-1]
+                if file[0] == "/":
+                        urlretrieve(BASEURL[0] + file, filename)
+                        print BASEURL[0] + file
+                elif file [0:2] == "pdf":
+                        urlretrieve(BASEURL[1] + file, filename)
+                        print BASEURL[1] + file
+                else:
+                        urlretrieve(file, filename)
+                        print file
+
+def pdf():
+        for link in soup.findAll(href = compile(types)):
+                file = link.get('href')
+                filename = file.split('/')[-1]
+                if file[0] == "/":
+                        urlretrieve(BASEURL[0] + file, filename)
+                        print BASEURL[0] + file
+                elif file [0:2] == "pdf":
+                        urlretrieve(BASEURL[1] + file, filename)
+                        print BASEURL[1] + file
+                else:
+                        urlretrieve(file, filename)
+                        print file
+
+def doc():
+        for link in soup.findAll(href = compile('\.doc$')):
+                file = link.get('href')
+                filename = file.split('/')[-1]
+                if file[0] == "/":
+                        try:
+                                #urlretrieve(BASEURL[0] + file, filename)
+                                print BASEURL[0] + file
+                        except:
+                                #urlretrieve(BASEURL[1] + file, filename)
+                                print BASEURL[1] + file
+                else:
+                        #urlretrieve(file, filename)
+                        print file
+
+def docx():
+        for link in soup.findAll(href = compile('\.docx$')):
+                file = link.get('href')
+                filename = file.split('/')[-1]
+                if file[0] == "/":
+                        try:
+                                #urlretrieve(BASEURL[0] + file, filename)
+                                print BASEURL[0] + file
+                        except:
+                                #urlretrieve(BASEURL[1] + file, filename)
+                                print BASEURL[1] + file
+                else:
+                        #urlretrieve(file, filename)
+                        print file
+
+def excel():
+        for link in soup.findAll(href = compile('\.xls$')):
+                file = link.get('href')
+                filename = file.split('/')[-1]
+                if file[0] == "/":
+                        try:
+                                #urlretrieve(BASEURL[0] + file, filename)
+                                print BASEURL[0] + file
+                        except:
+                                #urlretrieve(BASEURL[1] + file, filename)
+                                print BASEURL[1] + file
+                else:
+                        #urlretrieve(file, filename)
+                        print file
+
+def excelx():
+        for link in soup.findAll(href = compile('\.xlsx$')):
+                file = link.get('href')
+                filename = file.split('/')[-1]
+                if file[0] == "/":
+                        try:
+                                #urlretrieve(BASEURL[0] + file, filename)
+                                print BASEURL[0] + file
+                        except:
+                                #urlretrieve(BASEURL[1] + file, filename)
+                                print BASEURL[1] + file
+                else:
+                        #urlretrieve(file, filename)
+                        print file
+
+def jpg():
+        for link in soup.findAll(src = compile('\.jpg$')):
+                file = link.get('src')
+                filename = file.split('/')[-1]
+                if file[0] == "/":
+                        try:
+                                #urlretrieve(BASEURL[0] + file, filename)
+                                print url + ":  " + BASEURL[0] + file
+                        except:
+                                #urlretrieve(BASEURL[1] + file, filename)
+                                print url + ":  " + BASEURL[1] + file
+                else:
+                        #urlretrieve(file, filename)
+                        print url + ":  " + file
+
+def png():
+        for link in soup.findAll(src = compile('\.png$')):
+                file = link.get('src')
+                filename = file.split('/')[-1]
+                if file[0] == "/":
+                        try:
+                                #urlretrieve(BASEURL[0] + file, filename)
+                                print url + ":  " + BASEURL[0] + file
+                        except:
+                                #urlretrieve(BASEURL[1] + file, filename)
+                                print url + ":  " + BASEURL[1] + file
+                else:
+                        #urlretrieve(file, filename)
+                        print url + ":  " + file
+
+if __name__ == "__main__":
+        for url in URLs:
+                html_data = urlopen(url)
+                soup = BeautifulSoup(html_data)
+                
+                for types in FILETYPE:
+                        pdf()
+                #ppt()
+                #pptx()
+                #jpg()
+                #png()
+               # excel()
+               # excelx()
+               # doc()
+               # docx()
+"""
