fbc2e364417654b0cf72d070fde7f1c5a27f3b69
asdoylejr <asdoylejr@gmail.com>
2013-12-27 10:13:24 -0500

    streamlined main def

--- filedownloader.py
+++ filedownloader.py
@@ -3,6 +3,7 @@ from urllib import urlretrieve, urlopen
 from re import compile
 import os
 import sys
+import urlparse
 
 URLs = ['http://www1.eere.energy.gov/wind']
 
@@ -10,29 +11,32 @@ BASEURL = ['http://www1.eere.energy.gov/', 'http://www1.eere.energy.gov', 'http:
 
 FILETYPE = ['\.pdf$','\.ppt$', '\.pptx$', '\.doc$', '\.docx$', '\.xls$', '\.xlsx$', '\.wmv$']
 
-def main():
+def main(soup, domain, path, types):
 	for link in soup.findAll(href = compile(types)):
 		file = link.get('href')
 		filename = file.split('/')[-1]
+		
+		if '://' not in file and not file.startswith('//'):
+			if not file.startswith('/'):
+				file = urlparse.urljoin(path, file)
+			file = urlparse.urljoin(domain, file)
 		
 		try:
 			urlretrieve(file, filename)
 			print file
 		except:
-			for base in BASEURL:
-				try:
-					urlretrieve(base + file, filename)
-					print base + file
-					break
-				except:
-					print 'Error downloading ' + file
-					pass
+			print 'Error retrieving %s using URL %s' % (link.get('href'), file)
+		
 		
 if __name__ == "__main__":
 
 	for url in URLs:
 		html_data = urlopen(url)
 		soup = BeautifulSoup(html_data)
+		
+		urlinfo = urlparse.urlparse(url)
+		domain = urlparse.urlunparse((urlinfo.scheme, urlinfo.netloc, '', '', '', ''))
+		path = urlinfo.path.rsplit('/', 1)[0]
                 
 		for types in FILETYPE:
-			main()
+			main(soup, domain, path, types)
